8599f72215d0b1c698921bbfa4bba6af07f05426
Nisha gera <nishaa.1803gera@gmail.com>
2023-11-08 13:24:56 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+  FormsModule,
+  HttpClientModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
